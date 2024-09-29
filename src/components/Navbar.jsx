de706a65e0473bc1f77bd9389d5936cb01aad96a
Jae Hong Lee <ll.leejaehong.ll@gmail.com>
2024-09-29 18:02:24 -0400

    start Nav Bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,20 @@
 // src/components/Navbar.jsx
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css'; // Bootstrap 스타일 적용
+// Import CSS
+import './navbar.css'
+import './../index.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+// Import Bootstrap JavaScript
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
 function Navbar(){
-    return <h1>&quot;I swear by my pretty floral bonnet, I will end you.&quot;</h1>
+    return(
+        <nav className='navbar navbar-expand-lg fixed-top'>
+            <div className='container-fluid'>
+                <a className='navbar-brand navbar-component'>Jay</a>
+            </div>
+        </nav>
+    ) 
 }
 
 export default Navbar;
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+// Import  JavaScript
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import App from './App.jsx'
+
+// Import CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'
+
 import Navbar from './components/Navbar.jsx'
 
-// Import Bootstrap CSS
-import 'bootstrap/dist/css/bootstrap.min.css';
-// Import Bootstrap JavaScript
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 
 createRoot(document.getElementById('root')).render(
