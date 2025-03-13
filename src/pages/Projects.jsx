2e2ca1a5a0d146d69713dd7e47800c68402a7e9d
Jae Hong Lee <ll.leejaehong.ll@gmail.com>
2025-03-13 05:57:33 -0400

    brain done

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -13,14 +13,15 @@ function Projects() {
     const [activeProject, setActiveProject] = useState(null);
 
     const projectData = {
-        "CS1": { image: "images/project/brain-CS1.png", description: "Quantum Computing", position: { top: "30%", left: "15%" } },
-        "CS2": { image: "images/project/brain-CS2.png", description: "Machine Learning", position: { top: "35%", left: "17%" } },
-        "CS3": { image: "images/project/brain-CS3.png", description: "AI Research", position: { top: "45%", left: "18%" } },
-        "CS4": { image: "images/project/brain-CS4.png", description: "Cybersecurity", position: { top: "50%", left: "20%" } },
-        "Art1": { image: "images/project/brain-ART1.png", description: "Visual Art Portfolio", position: { top: "30%", left: "70%" } },
-        "Art2": { image: "images/project/brain-Art2.png", description: "Graphic Design", position: { top: "35%", left: "75%" } },
-        "Art3": { image: "images/project/brain-ART3.png", description: "Photography", position: { top: "45%", left: "78%" } },
-        "Art4": { image: "images/project/brain-ART4.png", description: "UI/UX Research", position: { top: "50%", left: "80%" } }
+        "CS1": { category : "CS", image: "images/project/brain-CS1.png", thumbnail : "images/project/thumbnail-CS1.svg",  title: "Quantum Computing", date: "August 2024", description: "The purpose of this project is to create a quantum simulator using Google's Tensor Network framework.", position: { top: "5%", left: "-5%" } },
+        "CS2": { category : "CS", image: "images/project/brain-CS2.png", thumbnail : "images/project/thumbnail-CS2.svg", title: "Hand Pose Estimation", date: "April 2024", description: "The purpose of this project is to create a quantum simulator using Google's Tensor Network framework.", position: { top: "25%", left: "-5%" } },
+        "CS3": { category : "CS", image: "images/project/brain-CS3.png", thumbnail : "images/project/thumbnail-CS1.svg", title: "AI Research", date: "August 2024", description: "The purpose of this project is to create a quantum simulator using Google's Tensor Network framework.", position: { top: "45%", left: "-5%" } },
+        "CS4": { category : "CS", image: "images/project/brain-CS4.png", thumbnail : "images/project/thumbnail-CS1.svg", title: "Cybersecurity", date: "August 2024", description: "The purpose of this project is to create a quantum simulator using Google's Tensor Network framework.",  position: { top: "55%", left: "-5%" } },
+
+        "Art1": { category : "Art", image: "images/project/brain-ART1.png", thumbnail : "images/project/thumbnail-Art1.svg", title: "Cogs and Gears", date: "March 2025", description: "The purpose of this project is to create a quantum simulator using Google's Tensor Network framework.",  position: { top: "5%", left: "75%" } },
+        "Art2": { category : "Art", image: "images/project/brain-Art2.png", thumbnail : "images/project/thumbnail-Art2.svg", title: "Visual Art Portfolio", date: "August 2023", description: "The purpose of this project is to create a quantum simulator using Google's Tensor Network framework.",  position: { top: "25%", left: "75%" } },
+        "Art3": { category : "Art", image: "images/project/brain-ART3.png", thumbnail : "images/project/thumbnail-Art3.svg", title: "Gill Sans", date: "August 2024", description: "The purpose of this project is to create a quantum simulator using Google's Tensor Network framework.", position: { top: "45%", left: "75%" } },
+        "Art4": { category : "Art", image: "images/project/brain-ART4.png", thumbnail : "images/project/thumbnail-Art4.svg", title: "Design Study", date: "August 2024", description: "The purpose of this project is to create a quantum simulator using Google's Tensor Network framework.",  position: { top: "55%", left: "75%" } }
     };
 
     const handleMouseEnter = (projectKey) => {
@@ -50,14 +51,20 @@ function Projects() {
                                 {/* Project Description (Appears Only when Hovering ) */}
                                 {activeProject && (
                                     <div
-                                        className='project-description'
+                                        className='project-template'
                                         style={{
                                             top: projectData[activeProject].position.top,
                                             left: projectData[activeProject].position.left
                                         }}
                                     >
-                                        <h2>{projectData[activeProject].description}</h2>
-                                        <p>Project Detail go here...</p>
+                                        <h2 className={`project-title ${projectData[activeProject].category === 'Art' ? 'art-title' : 'cs-title'}`}>{projectData[activeProject].title}</h2>
+                                        <p className='project-date'>{projectData[activeProject].date}</p>
+                                        <img 
+                                            src={projectData[activeProject].thumbnail} 
+                                            alt={projectData[activeProject].title} 
+                                            className="project-image"
+                                        />
+                                        <p className='project-description'>{projectData[activeProject].description}</p>
                                     </div>
                                 )}
                                 {/* Invisible Hover Areas */}
